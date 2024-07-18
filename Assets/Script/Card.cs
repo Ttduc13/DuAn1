75460def638b56fb8b9f380c625be62640ca43ed
Phat2509 <nguyenphat092005@gmail.com>
2024-07-19 00:22:51 +0700

    Card
    CARD

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+
+{
+    public string Name;
+    public string description;
+
+    public Sprite artwork;
+
+    public int manaCost;
+    public int attack;
+    public int health;
+
+
+
+    public void print()
+    {
+        Debug.Log(Name + ": " + description + " The card costs: " + manaCost);
+    }
+}
