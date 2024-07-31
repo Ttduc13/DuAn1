02a250a4316838fc636300faaed71400fbaa4d88
Ttduc13 <118754261+Ttduc13@users.noreply.github.com>
2024-07-31 23:09:43 +0700

    updateHealthBar

--- Assets/Scripts/Demon.cs
+++ Assets/Scripts/Demon.cs
@@ -17,6 +17,8 @@ public class Demon : MonoBehaviour
 
     private Animator anim;
 
+    public HealthBar healthBar;
+
     // skill
     public Transform skillPos;
     public GameObject[] prefab;
@@ -27,6 +29,8 @@ public class Demon : MonoBehaviour
     {
         player = FindObjectOfType<PlayerManager>();
         anim = GetComponent<Animator>();
+        currentHealth = health;
+        updateEnemyHelthBar();
     }
 
     void Update()
@@ -41,7 +45,13 @@ public class Demon : MonoBehaviour
         int damage = UnityEngine.Random.Range(minDamage, maxDamage);
         Debug.Log("Player take " + damage + " damage!");
         player.currentHealth = player.currentHealth - damage;
+        player.updatePlayerHelthBar();
         Instantiate(prefab[index], skillPos.position, Quaternion.identity);
         anim.SetBool("isAttack", false);
+    }
+
+    public void updateEnemyHelthBar()
+    {
+        healthBar.UpdateBar(currentHealth, health);
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,15 +7,23 @@ public class PlayerManager : MonoBehaviour
     public int health;
     public int currentHealth = 0;
     public int mana = 0;
+
+    public HealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = health;
+        updatePlayerHelthBar();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void updatePlayerHelthBar()
+    {
+        healthBar.UpdateBar(currentHealth, health);
     }
 }
