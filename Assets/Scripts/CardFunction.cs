a969884229a14241937a4e1cd7e904dcf4535659
Ttduc13 <118754261+Ttduc13@users.noreply.github.com>
2024-08-12 02:22:40 +0700

    Finale

--- Assets/Scripts/CardFunction.cs
+++ Assets/Scripts/CardFunction.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static Unity.Burst.Intrinsics.Arm;
-using static UnityEditor.Handles;
 
 public class CardFunction : MonoBehaviour
 {
--- Assets/Scripts/TipBoxManager.cs
+++ Assets/Scripts/TipBoxManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.VersionControl;
 
 public class TipBoxManager : MonoBehaviour
 {
